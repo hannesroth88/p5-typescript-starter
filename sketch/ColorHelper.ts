a4950ba0a508d5cdd7aeb88fff4df6b7d7be2f64
Roth Hannes (GS/BDA1) <Hannes.Roth2@de.bosch.com>
2023-04-08 21:27:49 +0200

    added more colors

--- sketch/ColorHelper.ts
+++ sketch/ColorHelper.ts
@@ -1,56 +1,102 @@
 class ColorHelper {
-    private static getColorVector(c: p5.Color) {
-        return createVector(
-            red(c),
-            green(c),
-            blue(c)
-        );
-    }
 
-    public static rainbowColorBase() {
+    public static customColors() {
         return [
-            color('red'),
-            color('orange'),
-            color('yellow'),
-            color('green'),
-            color(38, 58, 150), // blue
-            color('indigo'),
-            color('violet')
+            [
+                color(255, 211, 188),
+                color(255, 227, 209),
+                color(242, 218, 227),
+                color(203, 213, 225),
+                color(182, 224, 220),
+            ],
+            [
+                color(1, 51, 100),
+                color(334, 59, 90),
+                color(263, 58, 92),
+                color(200, 60, 90),
+                color(103, 58, 93)
+            ]
         ];
     }
 
-    public static getColorsArray(total: number, baseColorArray: p5.Color[] = null): p5.Color[] {
+    public static pasteleColors() {
+        return [
+            // Soft Pastels
+            [color(242, 192, 191), color(201, 209, 211), color(244, 229, 203), color(234, 212, 203), color(192, 193, 193)],
 
-        if (baseColorArray == null) {
-            baseColorArray = ColorHelper.rainbowColorBase();
-        }
-        var rainbowColors = baseColorArray.map(x => this.getColorVector(x));;
+            // Springtime
+            [color(176, 224, 230), color(240, 230, 140), color(135, 206, 250), color(152, 251, 152), color(255, 218, 185)],
 
-        let colours = new Array<p5.Color>();
-        for (var i = 0; i < total; i++) {
-            var colorPosition = i / total;
-            var scaledColorPosition = colorPosition * (rainbowColors.length - 1);
+            // Beach Day
+            [color(248, 236, 194), color(195, 226, 183), color(157, 193, 187), color(244, 180, 180), color(239, 239, 239)],
 
-            var colorIndex = Math.floor(scaledColorPosition);
-            var colorPercentage = scaledColorPosition - colorIndex;
+            // Ice Cream
+            [color(255, 229, 180), color(231, 241, 193), color(199, 206, 234), color(247, 178, 178), color(198, 210, 198)],
 
-            var nameColor = this.getColorByPercentage(rainbowColors[colorIndex],
-                rainbowColors[colorIndex + 1],
-                colorPercentage);
-
-            colours.push(color(nameColor.x, nameColor.y, nameColor.z))
-        }
-
-        return colours;
+            // Soft Hues
+            [color(247, 202, 201), color(146, 168, 209), color(166, 231, 240), color(247, 231, 206), color(231, 167, 206)]
+        ];
     }
 
-    private static getColorByPercentage(firstColor: p5.Vector, secondColor: p5.Vector, percentage: number) {
-        // assumes colors are p5js vectors
-        var firstColorCopy = firstColor.copy();
-        var secondColorCopy = secondColor.copy();
+    public static getRandomColorsPalette(total: number): p5.Color[] {
+        const colors = new Array<p5.Color>();
+        let baseColorArray: p5.Color[] = random(ColorHelper.customColors());
 
-        var deltaColor = secondColorCopy.sub(firstColorCopy);
-        var scaledDeltaColor = deltaColor.mult(percentage);
-        return firstColorCopy.add(scaledDeltaColor);
+        const originalColorArray = new Array<p5.Color>();
+        for (var i = 0; i < baseColorArray.length; i++) {
+            originalColorArray.push(baseColorArray[i])
+        }
+
+        if (total <= baseColorArray.length) {
+            for (var i = 0; i < total; i++) {
+                const iColor = floor(random(0, baseColorArray.length))
+                colors.push(baseColorArray[iColor])
+                baseColorArray.splice(iColor, 1)
+            }
+        } else {
+            for (var i = 0; i < total; i++) {
+                const iColor = floor(random(0, baseColorArray.length))
+                colors.push(baseColorArray[iColor])
+                // renew Array if empty
+                if (baseColorArray.length > 1) {
+                    baseColorArray.splice(iColor, 1)
+                } else {
+                    colors.push(random(originalColorArray))
+                }
+            }
+        }
+
+        return colors;
+    }
+    
+    public static getRandomPasteleColorsPalette(total: number): p5.Color[] {
+        const colors = new Array<p5.Color>();
+        let baseColorArray: p5.Color[] = random(ColorHelper.pasteleColors());
+
+        const originalColorArray = new Array<p5.Color>();
+        for (var i = 0; i < baseColorArray.length; i++) {
+            originalColorArray.push(baseColorArray[i])
+        }
+
+        if (total <= baseColorArray.length) {
+            for (var i = 0; i < total; i++) {
+                const iColor = floor(random(0, baseColorArray.length))
+                colors.push(baseColorArray[iColor])
+                baseColorArray.splice(iColor, 1)
+            }
+        } else {
+            for (var i = 0; i < total; i++) {
+                const iColor = floor(random(0, baseColorArray.length))
+                colors.push(baseColorArray[iColor])
+                // renew Array if empty
+                if (baseColorArray.length > 1) {
+                    baseColorArray.splice(iColor, 1)
+                } else {
+                    colors.push(random(originalColorArray))
+                }
+            }
+        }
+
+        return colors;
     }
 }
--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -1,47 +1,67 @@
+
 // GLOBAL VARS & TYPES
 let numberOfShapesControl: p5.Element;
+let numberOfShapesControlValue: p5.Element;
+const canvasSizeX = 800;
+const canvasSizeY = 800;
 
-// P5 WILL AUTOMATICALLY USE GLOBAL MODE IF A DRAW() FUNCTION IS DEFINED
+
+const numQuads = 4;
+const iterations = 4;
 function setup() {
   console.log("🚀 - Setup initialized - P5 is running");
 
-  createCanvas(windowWidth, windowHeight)
-  rectMode(CENTER).noFill().frameRate(30);
-  // NUMBER OF SHAPES SLIDER
-  numberOfShapesControl = createSlider(1, 30, 15, 1).position(10, 10).style("width", "100px");
-}
+  createCanvas(canvasSizeX, canvasSizeY)
+  rectMode(CENTER).noFill()
 
-// p5 WILL AUTO RUN THIS FUNCTION IF THE BROWSER WINDOW SIZE CHANGES
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+  // Sliders
+  // create a text object to display the slider value
+  numberOfShapesControlValue = createDiv();
+  numberOfShapesControl = createSlider(1, 30, 15, 1).position(10,10).style("width", "100px");
+  numberOfShapesControl.mouseReleased(drawRect)
+
 }
 
 // p5 WILL HANDLE REQUESTING ANIMATION FRAMES FROM THE BROWSER AND WIL RUN DRAW() EACH ANIMATION FROME
 function draw() {
-  
-   // CLEAR BACKGROUND
-  background(0);
+
 
   // CENTER OF SCREEN
-  translate(width / 2,height / 2);
+  translate(width / 2, height / 2);
+
+  drawRect();
+
+
+  noLoop();
+}
+
+function drawRect() {
+  // CLEAR BACKGROUND
+  background(0);
 
   const numberOfShapes = <number>numberOfShapesControl.value();
-  const colours = ColorHelper.getColorsArray(numberOfShapes);
+  const colors = ColorHelper.getRandomPasteleColorsPalette(numberOfShapes);
+
+  console.log({ numberOfShapes, colors: colors.length });
+  console.log(colors);
 
   // CONSISTENT SPEED REGARDLESS OF FRAMERATE
   const speed = (frameCount / (numberOfShapes * 30)) * 2;
-  
+
   // DRAW ALL SHAPES
   for (var i = 0; i < numberOfShapes; i++) {
     push();
-      const lineWidth = 8;
-      const spin = speed * (numberOfShapes - i);
-      const numberOfSides = 3 + i;
-      const width = 40 * i;
-      strokeWeight(lineWidth); 
-      stroke(colours[i]);
-      rotate(spin);
-      PolygonHelper.draw(numberOfSides, width)
+    const lineWidth = 8;
+    const spin = speed * (numberOfShapes - i);
+    const numberOfSides = 3 + i;
+    const width = 40 * i;
+    strokeWeight(lineWidth);
+    stroke(colors[i]);
+    rotate(spin);
+    PolygonHelper.draw(numberOfSides, width);
     pop();
   }
+  // display the slider value
+  numberOfShapesControlValue.html("Slider value: " + numberOfShapesControl.value());
+  
 }
